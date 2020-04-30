41dfed9de5ba7bfefd43ed7ee755e97510826461
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-04-29 21:24:59 -0500

    [Feature] Places view

---
+++ api/v1/views/places.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""view for Cities objects that handles all default RestFul API actions"""
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.place import Place
+from json import dumps
+from flask import abort, request, jsonify, make_response
+
+
+@app_views.route('/cities/<id>/places',
+                 methods=['GET'], strict_slashes=False)
+def places_by_city(id):
+    """Retrieves the list of all Places objects of a City"""
+    obj_list = []
+    all_cities = storage.all(City)
+    key = "City." + id
+    if key in all_cities:
+        places_list = all_cities[key].places
+    else:
+        abort(404)
+    for place in places_list:
+        obj = place.to_dict()
+        obj_list.append(obj)
+    obj_list = jsonify(obj_list)
+    return (obj_list)
+
+@app_views.route('/places/<id>',
+                 methods=['GET'], strict_slashes=False)
+def places_by_id(id):
+    """Retrieves the list of City by id"""
+    obj = None
+    all_places = storage.all(Place)
+    key = "Place." + id
+    if key in all_places:
+        obj = all_places[key].to_dict()
+    if obj is None:
+        abort(404)
+    obj = jsonify(obj)
+    return (obj)
+
+
+@app_views.route('/places/<id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_by_id(id):
+    """delete a place by id"""
+    dict_places = storage.all(Place)
+    key = "Place." + id
+    if key in dict_places:
+        obj = dict_places[key]
+        obj.delete()
+        storage.save()
+        return {}, 200
+    else:
+        abort(404)
+
+
+@app_views.route('cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
+def post_place(city_id):
+    """get request and post new Place"""
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    new_place = request.get_json()
+    all_cities = storage.all(City)
+    if not 'name' in new_place:
+        return abort(400, description="Missing name")
+    key = "City." + city_id
+    if not 'user_id' in new_place:
+        return abort(400, description="Missing user_id")
+    atributes = ['name', 'number_rooms', 'number_bathrooms', 'max_guest', 'price_by_night']
+    if key in all_cities:
+        obj_places = Place()
+        obj_places.city_id = city_id
+        for key in new_place:
+            if key in atributes:
+                setattr(obj_places, key, new_place[key])
+        obj_places.save()    
+        obj_places = obj_places.to_dict()
+        return make_response(jsonify(obj_places), 201)
+    else:
+        abort(404)
